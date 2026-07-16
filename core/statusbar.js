014096348b7385274c3897102d680b279efbab38
annaxiomm <amberisamab@gmail.com>
2026-07-16 19:43:29 +1200

    windows can now be dragged and closed !

--- core/statusbar.js
+++ core/statusbar.js
@@ -1,5 +1,11 @@
+import { windows } from "./wm";
+
 export function updateStatusBar() {
   let now = new Date();
   let formatted_date = now.toLocaleString();
   document.getElementById("status_time").innerText = formatted_date;
 }
+
+document.getElementById("os_name").addEventListener("click", () => {
+  windows["welcome"].openWindow();
+});
--- main.js
+++ main.js
@@ -1,10 +1,21 @@
 import { updateStatusBar } from "./core/statusbar";
+import { initCaribou } from "./core/wm";
 
 setInterval(() => {
   fixedUpdate();
 }, 1000);
 
+document.addEventListener("DOMContentLoaded", () => {
+  onStart();
+});
+
 // runs once every second (1000ms)
 function fixedUpdate() {
   updateStatusBar();
+}
+
+// runs when the "OS" loads
+function onStart() {
+  // initialise the window managers
+  initCaribou();
 }
