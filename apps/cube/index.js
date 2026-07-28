727a5a58c304a5df41189fb20f51ad35683e588f
annaxiomm <amberisamab@gmail.com>
2026-07-28 14:50:32 +1200

    fix: keep trying to fix the cube

--- apps/cube/index.js
+++ apps/cube/index.js
@@ -14,7 +14,7 @@ export class CubeApp extends Window {
     }, windowID)
 
     const scene = new Scene();
-    const camera = new PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+    const camera = new PerspectiveCamera(75, 1, 0.1, 1000);
 
     const renderer = new WebGLRenderer({
       antialias: true
