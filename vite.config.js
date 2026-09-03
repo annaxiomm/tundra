4320657f2ce2d8ca9f889793d936d478e6d6f442
amberisamab@gmail.com <amberisamab@gmail.com>
2026-09-04 10:21:37 +1200

    browser: fix homepage

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
 import { defineConfig } from 'vite'
 import mkcert from 'vite-plugin-mkcert'
+import { resolve } from 'path'
 
 export default defineConfig({
   base: "/tundra/",
   plugins: [
     mkcert(),  // ← Enables HTTPS locally
   ],
+  input: {
+    main: resolve(import.meta.dirname, 'index.html'),
+    homepage: resolve(import.meta.dirname, 'homepage.html')
+  }
 })
